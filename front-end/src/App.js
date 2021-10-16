07eb5d1d2e2aa83f4c905362a30ff6d1cc2c11ec
ByJhona <jhonatantecno23@gmail.com>
2021-10-16 15:57:32 -0300

    Adicionada tela de game

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login/Login'
+import Game from './Components/Game/Game'
 
 
 function App() {
   return (
     
-      <Login/>
+      <Game />
     
   );
 }
