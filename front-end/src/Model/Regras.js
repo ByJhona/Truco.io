3875f74f8054dd68d11454d4541b6f01cc1f6099
Pecximenes <pedroximenes76@gmail.com>
2021-10-24 19:53:48 -0300

    Inserção da função que analisa o ganhador da partida

--- front-end/src/Model/Regras.js
+++ front-end/src/Model/Regras.js
@@ -45,12 +45,42 @@ export function distribuiCartas(nameRoom, database){
     update(ref(database, `/rooms/${nameRoom}/player2`), {deck});   
 }
 
+export function aceitarOuCorrer(player ,nameRoom, database){
+    //confirm('Oi')
+}
+
+/**
+ * @param  {} player
+ * @param  {} nameRoom
+ * @param  {} database
+ */
 export function pedirTruco(player, nameRoom, database){
     //pedir o truco
 
+    update(ref(database, `/rooms/${nameRoom}/desk/truco`), {whoAsk: player})
+    get(ref(database, `/rooms/${nameRoom}`)).then((data)=>{
+        const mesa = data.val()
+        const player1 = mesa.player1.nickname
+        const player2 = mesa.player2.nickname
+
+        if (player == player1){
+            var playerWhoNeedAccepted = player2
+            aceitarOuCorrer(playerWhoNeedAccepted, nameRoom, database)
+
+        }else if(player == player2){
+            var playerWhoNeedAccepted = player1
+            aceitarOuCorrer(playerWhoNeedAccepted, nameRoom, database)
+        }})
     
+
 }
 
+/**
+ * @param  {} playerTruco
+ * @param  {} playerAceita
+ * @param  {} nameRoom
+ * @param  {} database
+ */
 export function verificaTruco(playerTruco, playerAceita, nameRoom, database){
     //validador booleano do truco
 
@@ -58,4 +88,56 @@ export function verificaTruco(playerTruco, playerAceita, nameRoom, database){
         update(ref(database, `/rooms/${nameRoom}/desk/truco`), {trucoStatus: true});
         update(ref(database, `/rooms/${nameRoom}/desk`), {scoreRound: 3});
     }
+}
+
+function identificarQualJogador(winPlayer, player1, player2){
+    if (winPlayer == player1){
+        return 'player1'
+
+    }else if (winPlayer == player2){
+        return 'player2'
+
+    }
+}
+
+export function verificaQuemGanha(nameRoom, database){
+    get(ref(database, `/rooms/${nameRoom}`)).then((data)=>{
+        const sala = data.val()
+        const round1 = sala.desk.round1
+        const round2 = sala.desk.round2
+        const round3 = sala.desk.round3
+        const empate = 'empate'
+        const winList = [round1, round2, round3]
+        const point = sala.desk.scoreRound
+
+        const player1Points = sala.player1.pontos
+        const player2Points = sala.player2.pontos
+        const player1 = sala.player1.nickname
+        const player2 = sala.player2.nickname
+
+        if (winList.filter(x == player1) > winList.filter(x == player2)){
+            var winPoint = point + player1Points
+            update(ref(database, `/rooms/${nameRoom}/player1`), {pontos: winPoint})
+
+        }else if (winList.filter(x == player1) > winList.filter(x == player2)){
+            var winPoint = point + player2Points
+            update(ref(database, `/rooms/${nameRoom}/player2`), {pontos: winPoint})
+
+        }else if (winList[0] == empate && winList[1] != empate){
+            const player = identificarQualJogador(winList[1], player1, player2)
+            update(ref(database, `/rooms/${nameRoom}/${player}`), {pontos: winPoint})
+
+        }else if (winList[1] == empate && winList[0] != empate){
+            const player = identificarQualJogador(winList[0], player1, player2)
+            update(ref(database, `/rooms/${nameRoom}/${player}`), {pontos: winPoint})
+
+        }else if (winList[0] == empate && winList[1] == empate && winList[2] != empate){
+            const player = identificarQualJogador(winList[2], player1, player2)
+            update(ref(database, `/rooms/${nameRoom}/${player}`), {pontos: winPoint})
+
+        }else if (winList[2] == empate && winList[0] != empate){
+            const player = identificarQualJogador(winList[0], player1, player2)
+            update(ref(database, `/rooms/${nameRoom}/${player}`), {pontos: winPoint})            
+
+        }})
 }
